889d6f72c96c71f6c58044402fce08780c4c681a
Gus Antoniassi <gus.antoniassi@gmail.com>
2018-05-19 15:03:41 -0300

    Classe de notificação passada para Kotlin

---
+++ app/src/main/java/com/melhorgrupo/printserverclient/MyMessagingService.kt
@@ -0,0 +1,68 @@
+package com.melhorgrupo.printserverclient
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyMessagingService : FirebaseMessagingService() {
+    private val TAG : String = "MyMessagingService"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        super.onMessageReceived(remoteMessage)
+
+        if (remoteMessage == null) {
+            Log.e(TAG, "remoteMessage veio null");
+            return
+        }
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
+
+        Log.d(TAG, "From: " + remoteMessage.from!!)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
+
+            val channelId = getString(R.string.default_notification_channel_id)
+            val appName = getString(R.string.app_name)
+
+            val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+            val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                    .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                    .setContentTitle(appName)
+                    .setContentText(remoteMessage.notification!!.body)
+                    .setSound(defaultSoundUri)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            // Since android Oreo notification channel is needed.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(channelId,
+                        "Channel human readable title",
+                        NotificationManager.IMPORTANCE_DEFAULT)
+                notificationManager.createNotificationChannel(channel)
+            }
+
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+        }
+    }
+}
